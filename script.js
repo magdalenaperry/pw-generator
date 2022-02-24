60fa68484de689f20e14c9932aa80c4424b65242
Magdalena Perry <magperry16@gmail.com>
2022-02-24 14:17:47 -0500

    update css file

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// click button = generate password
-// what do i need for my password? criteria
-// click on the criteria needed for password
-// determine the length of my password (conditions 8-128 char)
-// what characters do I want in my password? (select true/false: lowercase, uppercase, numbers, or spec. char)
-// must have one true selector
-// dom manipulation methods password to display
-
-
-// dom manipulation methods
-// functions that are built in to javascript
-// 
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -27,4 +14,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+// click button = generate password
+// what do i need for my password? criteria
+// click on the criteria needed for password
+// determine the length of my password (conditions 8-128 char)
+// what characters do I want in my password? (select true/false: lowercase, uppercase, numbers, or spec. char)
+// must have one true selector
+// dom manipulation methods password to display
+
+
+// dom manipulation methods
+// functions that are built in to javascript
+// 
 
