b96c5ba8cd7d6cf172c0bcc0cfaad7df90a30bd9
Magdalena Perry <magperry16@gmail.com>
2022-02-27 19:35:50 -0500

    updated range 8-128 characters

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+  var special = "!@#$%^&*(){}|:<.,>?-+/';']/";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var number = "0123456789";
+
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  console.log(count);
+
+  // count greater than 8 or less than 128
+  if (count < 8 || count > 128){
+      alert('Please choose between 8 and 128 characters.');
+      return "";
+  }
+
+  
+
+  var genPassword = "";
+  for (var i = 0; i < count; i++){
+      var random = Math.floor(Math.random() * letters.length);
+      genPassword += letters[Math.floor(Math.random() * letters.length)];
+  }
+  
+  return genPassword;
+
+  // var specialChar = Number(prompt("Would you like to include special characters?"));
+  // console.log(specialChar);
+
+
+  // for (var i = 0; i < count, i++){
+  //   var random = Math.floor(Math.random() * special.length);
+  //   genPassword += special[Math.floor(Math.random() * special.length)];
+  // }
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -16,17 +52,4 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-// click button = generate password
-// what do i need for my password? criteria
-// click on the criteria needed for password
-// determine the length of my password (conditions 8-128 char)
-// what characters do I want in my password? (select true/false: lowercase, uppercase, numbers, or spec. char)
-// must have one true selector
-// dom manipulation methods password to display
-
-
-// dom manipulation methods
-// functions that are built in to javascript
-// 
 
